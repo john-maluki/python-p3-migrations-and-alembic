861212558913b3fa4d7c66e8d988d3266b0b0dfb
john <johnmaluki58@gmail.com>
2023-08-30 22:12:47 +0300

    Complete assignment on Migrations and Alembic

---
+++ app/migrations/versions/67821a56092d_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 67821a56092d
+Revises: 
+Create Date: 2023-08-30 21:53:25.967103
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '67821a56092d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
